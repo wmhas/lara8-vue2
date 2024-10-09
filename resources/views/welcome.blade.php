b87fd3493e40bfbcf8235d382d79c54d8fa6377d
whashim <whashim@gmail.com>
2024-10-09 21:21:29 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+ <html>
+    <head>
+        <title>My First Laravel App</title>
+       
+    <body>
+        
+        <h1>Welcome to my first Laravel App</h1>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    </head>
+    
+    </body>
+ </html>
